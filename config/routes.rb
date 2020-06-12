e475478cebce2684a785bca3ddf1f2639bba18a5
Michael Pope <michael@dtcorp.com.au>
2020-06-12 11:42:17 +1000

    DeviseToken-FEAT-Properly namespace API calls

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users             # Standard devise routes must come first!
-  mount_devise_token_auth_for 'User', at: 'auth'
+
+  namespace :api, defaults: {format: 'json'} do # namespace devise token routes to stop duplication
+    namespace :v1 do
+      mount_devise_token_auth_for 'User', at: 'auth'
+    end
+  end
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/home", to: "home#index"
   root to: "home#index"
