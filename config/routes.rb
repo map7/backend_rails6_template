47fb57acd9003d0619e20fa9b369afb05b95e90c
Michael Pope <michael@dtcorp.com.au>
2020-06-18 09:28:30 +1000

    Route-FEAT-Change get to resources

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,6 @@ Rails.application.routes.draw do
   end
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "/home", to: "home#index"
+  resources :home, only: [:index]
   root to: "home#index"
 end
