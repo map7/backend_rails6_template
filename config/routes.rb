93342194a9c8d056a3fc4ca280d1eb4abedcd7d0
Michael Pope <michael@dtcorp.com.au>
2021-04-22 11:29:54 +1000

    DeviseToken-FEAT-Add API route for home

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users             # Standard devise routes must come first!
+  resources :home, only: [:index]
+  root to: "home#index"
 
   namespace :api, defaults: {format: 'json'} do # namespace devise token routes to stop duplication
     namespace :v1 do
       mount_devise_token_auth_for 'User', at: 'auth'
+      resources :home, only: [:index]
     end
   end
-  
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :home, only: [:index]
-  root to: "home#index"
 end
