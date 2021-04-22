ef8c4ebfac6c7ad2e6bd03dc8a1595bba228d7e2
Michael Pope <michael@dtcorp.com.au>
2021-04-22 11:29:39 +1000

    DeviseToken-FEAT-Simplify the old home controller for HTML views

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,3 @@
 class HomeController < ApplicationController
   before_action :authenticate_user!
-
-  respond_to :html, :json
-  
-  def index
-    respond_to do |format|
-      format.html
-      format.json { render json: {message: "Welcome to the Ruby on Rails backend"} }
-    end
-  end
 end
